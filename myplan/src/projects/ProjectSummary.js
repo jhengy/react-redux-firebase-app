00094b902521498668f755e87cdcfbc0bf0e0d49
jhengy <35480886+jhengy@users.noreply.github.com>
2019-05-29 15:29:43 +0800

    Abstract each project summary in the project list

---
+++ myplan/src/projects/ProjectSummary.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// a component entry for the project list of dashboard
+const ProjectSummary = () => {
+  return (
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project title</span>
+          <p>Posted by Hengyuan</p>
+          <p className="grey-text">29 May 2019</p>
+        </div>
+      </div>
+  )
+}
+
+export default ProjectSummary
